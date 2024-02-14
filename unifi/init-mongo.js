dd1534b7304a986798edd88e6d13999545c82f68
LAurentiu Gosu <laurentiu.gosu@gmail.com>
2024-02-14 18:57:23 +0200

    Corrected init-mongo.js

--- unifi/init-mongo.js
+++ unifi/init-mongo.js
@@ -1,1 +1,3 @@
-db.getSiblingDB("unifi").createUser({user: "unifi", pwd: "CHANGEME", roles: [{role: "dbOwner", db: "unifi"}, {role: "dbOwner", db: "unifi_stat"}]});
+db.getSiblingDB("unifi").createUser({user: "unifi", pwd: "REPLACEME", roles: [{role: "dbOwner", db: "unifi"}]});
+db.getSiblingDB("unifi_stat").createUser({user: "unifi", pwd: "REPLACEME", roles: [{role: "dbOwner", db: "unifi_stat"}]});
+
